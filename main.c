6d67c98780018e199324ec4a1d4210ba39724b4a
azaika <takaaza@gmail.com>
2020-09-25 18:31:16 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int mysh_init(void) {
+    return 0;
+}
+
+int mysh_loop(void) {
+    return 0;   
+}
+
+int mysh_terminate(void) {
+    return 0;   
+}
+
+int main(void) {
+    int init_err = mysh_init();
+    if (init_err) {
+        printf("mysh: error occured in initialization process.\n");
+        return EXIT_FAILURE;
+    }
+
+    int loop_err = mysh_loop();
+    if (loop_err) {
+        printf("mysh: error occured in loop process.\n");
+        return EXIT_FAILURE;
+    }
+
+    int terminate_err = mysh_terminate();
+    if (terminate_err) {
+        printf("mysh: error occured in loop process.\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("mysh: byebye 👋");
+
+    return EXIT_SUCCESS;
+}
