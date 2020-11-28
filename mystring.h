7d3bb5c0f94ae1417f8a773824e69ccd21e904ab
azaika <takaaza@gmail.com>
2020-11-28 13:11:43 +0900

    refine mystring.h

--- mystring.h
+++ mystring.h
@@ -5,71 +5,16 @@
 #include <string.h>
 #include <assert.h>
 
-typedef struct {
+typedef struct mysh_string_tag {
     char* ptr;
     size_t length;
     size_t capacity;
 } mysh_string;
 
-static void ms_extend(mysh_string* s, size_t size) {
-    assert(s != NULL);
-
-    while (s->capacity < size) {
-        s->capacity *= 2;
-    }
-    
-    s->ptr = realloc(s->ptr, s->capacity * sizeof(char));
-}
-
-static void ms_init(mysh_string* s, const char* src) {
-    if (s == NULL || src == NULL)
-        return;
-
-    size_t size = strlen(src) + 1;
-
-    size_t cap = 8;
-    while (cap < size) {
-        cap *= 2;
-    }
-    
-    s->ptr = (char*)malloc(cap * sizeof(char));
-    s->length = size;
-    s->capacity = cap;
-
-    strcpy(s->ptr, src);
-}
-
-static void ms_assign(mysh_string* s1, const mysh_string* s2) {
-    assert(s1 != NULL);
-
-    if (s1->ptr == NULL) {
-        ms_init(s1, s2->ptr);
-        return;
-    }
-
-    ms_extend(s1, s2->length);
-    s1->length = s2->length;
-
-    strcpy(s1->ptr, s2->ptr);
-}
-
-static void ms_assign_raw(mysh_string* str, const char* src) {
-    if (str->ptr == NULL) {
-        ms_init(str, src);
-        return;
-    }
-
-    size_t len = strlen(src) + 1;
-    ms_extend(str, len);
-    str->length = len;
-
-    strcpy(str->ptr, src);
-}
-
-static size_t ms_reserve(mysh_string* s, size_t new_cap) {
+static size_t ms_reserve(mysh_string* s, size_t len) {
     assert(s != NULL);
     
-    while (s->capacity < new_cap) {
+    while (s->capacity < len + 1) {
         s->capacity *= 2;
     }
 
@@ -80,7 +25,64 @@ static size_t ms_reserve(mysh_string* s, size_t new_cap) {
         s->ptr = realloc(s->ptr, s->capacity * sizeof(char));
     }
 
+    if (s->ptr == NULL) {
+        fprintf(stderr, "mysh: error occurred in allocation.\n");
+        exit(EXIT_FAILURE);
+    }
+
     return s->capacity;
+}
+
+static void ms_init(mysh_string* str, const char* src) {
+    if (str == NULL || src == NULL || str->ptr != NULL)
+        return;
+
+    size_t len = strlen(src);
+
+    size_t cap = 8;
+    while (cap < len + 1) {
+        cap *= 2;
+    }
+    
+    str->ptr = (char*)malloc(cap * sizeof(char));
+    if (str->ptr == NULL) {
+        fprintf(stderr, "mysh: error occurred in allocation.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    str->length = len;
+    str->capacity = cap;
+
+    strcpy(str->ptr, src);
+}
+
+static void ms_assign(mysh_string* s1, const mysh_string* s2) {
+    assert(s1 != NULL);
+
+    if (s1->ptr == NULL) {
+        ms_init(s1, s2->ptr);
+        return;
+    }
+
+    ms_reserve(s1, s2->length);
+    s1->length = s2->length;
+
+    strcpy(s1->ptr, s2->ptr);
+}
+
+static void ms_assign_raw(mysh_string* str, const char* src) {
+    assert(str != NULL);
+
+    if (str->ptr == NULL) {
+        ms_init(str, src);
+        return;
+    }
+
+    size_t len = strlen(src);
+    ms_reserve(str, len);
+    str->length = len;
+
+    strcpy(str->ptr, src);
 }
 
 static void ms_shrink(mysh_string* s) {
@@ -91,13 +93,23 @@ static void ms_shrink(mysh_string* s) {
     }
 
     s->ptr = realloc(s->ptr, s->capacity * sizeof(char));
+
+    if (s->ptr == NULL) {
+        fprintf(stderr, "mysh: error occurred in allocation.\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 static void ms_clear(mysh_string* s) {
     assert(s != NULL);
 
-    s->ptr[0] = '\0';
-    s->length = 0;
+    if (s->ptr == NULL) {
+        ms_init(s, "");
+    }
+    else {
+        s->ptr[0] = '\0';
+        s->length = 0;
+    }
 }
 
 static void ms_relase(mysh_string* s) {
@@ -107,6 +119,55 @@ static void ms_relase(mysh_string* s) {
     s->ptr = NULL;
     s->length = 0;
     s->capacity = 0;
+}
+
+static void ms_push(mysh_string* s, char c) {
+    assert(s != NULL);
+
+    if (s->ptr == NULL) {
+        ms_init(s, "");
+    }
+
+    ms_reserve(s, s->length + 1);
+
+    s->ptr[s->length] = c;
+    s->ptr[s->length + 1] = '\0';
+
+    ++s->length;
+}
+
+static void ms_concat(mysh_string* s1, const mysh_string* s2) {
+    assert(s1 != NULL && s2 != NULL);
+
+    if (s2->ptr == NULL) {
+        return;
+    }
+    if (s1->ptr == NULL) {
+        ms_init(s1, s2->ptr);
+        return;
+    }
+
+    ms_reserve(s1, s1->length + s2->length);
+
+    strcpy(s1->ptr + s1->length, s2->ptr);
+
+    s1->length += s2->length;
+}
+
+static void ms_concat_raw(mysh_string* str, const char* src) {
+    assert(str != NULL && src != NULL);
+
+    if (str->ptr == NULL) {
+        ms_init(str, src);
+        return;
+    }
+
+    size_t len = strlen(src);
+    ms_reserve(str, str->length + len);
+
+    strcpy(str->ptr + str->length, src);
+
+    str->length += len;
 }
 
 #endif // MYSH_MYSTRING_H
