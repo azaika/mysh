54776bff2e72d49edb40a064f25beb761276eb68
azaika <takaaza@gmail.com>
2020-11-28 18:23:58 +0900

    implement tokenizer

--- shell_resource.h
+++ shell_resource.h
@@ -7,7 +7,7 @@
 
 #include "mystring.h"
 
-typedef struct {
+typedef struct mysh_resource_tag {
     mysh_string current_dir;
     mysh_string home_dir;
 } mysh_resource;
@@ -19,7 +19,7 @@ static void mysh_set_curdir_name(const char* name) {
         return;
     }
 
-    size_t name_len = strlen(name) + 1;
+    size_t name_len = strlen(name);
     if (name_len < mysh_shell_resource.home_dir.length) {
         ms_assign_raw(&mysh_shell_resource.current_dir, name);
         return;
@@ -27,7 +27,7 @@ static void mysh_set_curdir_name(const char* name) {
 
     int flag = 1;
     size_t i;
-    for (i = 0; i + 1 < mysh_shell_resource.home_dir.length; ++i) {
+    for (i = 0; i < mysh_shell_resource.home_dir.length; ++i) {
         if (name[i] != mysh_shell_resource.home_dir.ptr[i]) {
             flag = 0;
             break;
@@ -35,7 +35,7 @@ static void mysh_set_curdir_name(const char* name) {
     }
 
     if (flag) {
-        ms_extend(&mysh_shell_resource.current_dir, name_len - i + 1);
+        ms_reserve(&mysh_shell_resource.current_dir, name_len - i + 1);
         mysh_shell_resource.current_dir.length = name_len - i + 1;
 
         mysh_shell_resource.current_dir.ptr[0] = '~';
